f28fbd2deae553092dfe21c0885eec83db9baa7d
Or Zarchi <or@whisp.software>
2019-03-16 16:58:46 +0200

    Good luck candidate!

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className='app'>
+                <div className="page-content">
+                    <h1>What are you waiting for?</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
